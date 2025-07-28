4066b5ac2ec20491269b39b98d48f239ffee190b
Gallardo2C-IT <chadromanus@gmail.com>
2025-07-28 17:50:41 +0800

    Ambot lang ani oie

---
+++ Gallardo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gallardo;
+
+/**
+ *
+ * @author Jade
+ */
+public class Gallardo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
